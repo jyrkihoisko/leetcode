1e6519a65bd5542e4eb1e6edb583b319751d85a6
Jyrki Hoisko <jyrki.hoisko@gmail.com>
2020-08-16 09:03:21 -0700

    Minimum Operations to Make Array Equal
    "You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid values of i (i.e. 0 <= i < n).
    
    In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and add 1 to arr[y] (i.e. perform arr[x] -=1 and arr[y] += 1). The goal is to make all the elements of the array equal. It is guaranteed that all the elements of the array can be made equal using some operations.
    
    Given an integer n, the length of the array. Return the minimum number of operations needed to make all the elements of arr equal."
    --------
    Solution:
    Instead of relying on mathematical equations, we just calculate the matrix and check the median value.
    Then calculate the difference of each array item to that median.

--- XCode Playgrounds/MinOperations.playground/Contents.swift
+++ XCode Playgrounds/MinOperations.playground/Contents.swift
@@ -1,3 +1,51 @@
-import UIKit
+class Solution {
+    func minOperations(_ n: Int) -> Int {
+        guard n > 1 else { return 0 }
+        var ret = 0
+        var arr = Array(repeating: 0, count: n)
+        for i in 0..<n {
+            arr[i] = (2 * i) + 1
+        }
+        let middle = arr.count / 2
+        let median = (arr.count % 2 == 0) ? ((arr[middle] + arr[middle-1]) / 2) : arr[middle]
+        print(arr)
+        print(median)
+        
+        for i in 0..<middle {
+            ret += median - arr[i]
+        }
+        print("return: \(ret)")
+        return ret
+    }
+}
 
-var str = "Hello, playground"
+
+
+let s = Solution()
+
+let i = 3, o = 2
+let i2 = 4, o2 = 4
+let i3 = 0, o3 = 0
+let i4 = 1, o4 = 0
+let i5 = 10000, o5 = 100
+assert(s.minOperations(i) == o)
+assert(s.minOperations(i2) == o2)
+assert(s.minOperations(i3) == o3)
+assert(s.minOperations(i4) == o4)
+assert(s.minOperations(i5) == 25000000)
+
+print("Tests done!")
+
+
+/*
+ arr[i] = (2 * i) + 1
+
+
+ In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and add 1 to arr[y] (i.e. perform arr[x] -=1 and arr[y] += 1). The goal is to make all the elements of the array equal. It is guaranteed that all the elements of the array can be made equal using some operations.
+
+ Given an integer n, the length of the array. Return the minimum number of operations needed to make all the elements of arr equal.
+
+  
+
+ */
+
