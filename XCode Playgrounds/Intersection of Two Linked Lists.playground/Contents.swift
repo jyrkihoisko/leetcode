05ca6fb3297af84ea293514c0ac54caac92e341f
Jyrki Hoisko <jyrki.hoisko@gmail.com>
2020-09-05 11:13:51 -0700

    Intersection of Two Linked Lists
    "Write a program to find the node at which the intersection of two singly linked lists begins."
    
    -----
    Solution:
    Approach from Computer Science theory.
    Traverse both lists twice until pointers are at the end; then continue from the OTHER list.
    Continue until both pointers are nil (linked lists do not intersect) or the pointers are the
    same (at the intersection). SIMPLE and FAST.

--- XCode Playgrounds/Intersection of Two Linked Lists.playground/Contents.swift
+++ XCode Playgrounds/Intersection of Two Linked Lists.playground/Contents.swift
@@ -1,61 +1,12 @@
 class Solution {
-    // Intuition -driven approach.
-    // Works; is comparatively fast, but overly too complex.
-    
+    // From LeetCode:
     func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
-        
-        if headA == nil || headB == nil { return nil }
-        if headA === headB { return headA }
-        
-        let aL = length(headA)
-        let bL = length(headB)
-        // print("A length: \(aL.0); B length: \(bL.0)")
-        if aL.1 !== bL.1 { return nil }
-        let rA = reverse(headA)
-        let nBL = length(headB)
-        // print(" new B length: \(nBL.0)")
-        print("location of intersect: \((aL.count - nBL.count + bL.count) / 2) from end")
-        reverse(rA)
-        
-        // Traverse to nth node from end (two pointers -method)
-        var ptrA = headA
-        var ptrB = ptrA
-        
-        for _ in 0..<(aL.count - nBL.count + bL.count) / 2 {
-            ptrA = ptrA?.next
+        var pa = headA, pb = headB
+        while pa !== pb {
+            pa = (pa != nil) ? pa!.next : headB
+            pb = (pb != nil) ? pb!.next : headA
         }
-        while ptrA?.next != nil {
-            ptrA = ptrA?.next
-            ptrB = ptrB?.next
-        }
-        print("ptrB points to \(ptrB!.val)")
-        return ptrB
-    }
-    
-    func length(_ head: ListNode?) -> (count: Int, tail: ListNode?) {
-        var count = 0
-        var n = head
-        var prevHead: ListNode?
-        while n != nil {
-            prevHead = n
-            n = n?.next
-            count += 1
-        }
-        print("head starting at \(head!.val) count \(count - 1)")
-        return (count - 1, prevHead)
-    }
-    
-    func reverse(_ head: ListNode?) -> ListNode? {
-        var prev: ListNode?
-        var next: ListNode?
-        var n = head
-        while n != nil {
-            next = n?.next
-            n?.next = prev
-            prev = n
-            n = next
-        }
-        return prev
+        return pa
     }
 }
 
